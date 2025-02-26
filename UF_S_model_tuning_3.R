4a42e9d815cc9c7070f5eb30775d1cf001098e09
amh312 <127774411+amh312@users.noreply.github.com>
2025-02-26 13:13:56 +0000

    Update UF_S_model_tuning_3.R

--- UF_S_model_tuning_3.R
+++ UF_S_model_tuning_3.R
@@ -3,10 +3,34 @@
 set.seed(123)
 
 ###Third round of hyperparameter tuning (learning rate)
-parameter_list <- c(0.1,0.05,0.01)
-best_auc <- 0
+
+###Read_in previous parameters
+file_names <- glue("col_sub_{combined_antimicrobial_map}.csv")
+col_sub_bestparams <- lapply(file_names, function(file) {
+  read_csv(file)
+})
+namelist <- c("eta","max_depth","min_child_weight","subsample","colsample_bytree",
+              "best_nrounds")
+for (i in 1:length(col_sub_bestparams)) {
+  names(col_sub_bestparams[[i]])[3:8] <- namelist
+}
+names(col_sub_bestparams) <- combined_antimicrobial_map
+
+file_names <- glue("max_child_{combined_antimicrobial_map}.csv")
+max_child_bestparams <- lapply(file_names, function(file) {
+  read_csv(file)
+})
+namelist <- c("eta","max_depth","min_child_weight","subsample","colsample_bytree",
+              "best_nrounds")
+for (i in 1:length(max_child_bestparams)) {
+  names(max_child_bestparams[[i]])[3:8] <- namelist
+}
+names(max_child_bestparams) <- combined_antimicrobial_map
+
 final_bestparams <- c()
 for (outcome in colnames(urines5_outcomes)) {
+  
+  best_auc <- 0
   
   if (sum(!is.na(urines5_combined[[outcome]])) > 0) {
     
@@ -26,14 +50,18 @@ for (outcome in colnames(urines5_outcomes)) {
     micro_matrix <- xgb.DMatrix(data = as.matrix(ur_xg_combined %>% select(all_of(selected_columns))), 
                                 label = ur_xg_combined[[outcome]])
     
-    for (i in 1:length(parameter_list)) {
+    n_rounds_run <- 0
+    parameter_val <- 0.1
+    i <- 1
+    
+    while(n_rounds_run<200|n_rounds_run==1000) {
       
       print(glue("Running CV {i} for {outcome}..."))
       
       params <- list(
         objective = "binary:logistic",
         eval_metric = "auc",
-        eta = parameter_list[i],
+        eta = parameter_val,
         max_depth = max_child_bestparams[[outcome]]$max_depth,
         min_child_weight = max_child_bestparams[[outcome]]$min_child_weight,
         subsample = col_sub_bestparams[[outcome]]$subsample,
@@ -52,11 +80,25 @@ for (outcome in colnames(urines5_outcomes)) {
       best_iteration_index <- which.max(cv_model$evaluation_log$test_auc_mean)
       best_iteration_auc <- cv_model$evaluation_log$test_auc_mean[best_iteration_index]
       cv_model$evaluation_log$test_logloss_mean
-      if (best_iteration_auc > best_auc) {
+      
         best_auc <- best_iteration_auc
         best_params <- params
         best_nrounds <- best_iteration_index
+      
+      
+      n_rounds_run <- cv_model$evaluation_log %>% nrow()
+      
+      if(n_rounds_run<200) {
+        
+        parameter_val <- parameter_val/2
+        
+      } else if (n_rounds_run==1000) {
+        
+        parameter_val <- parameter_val+0.1
+        
       }
+      
+      i <- i+1
       
     }
     
@@ -65,6 +107,7 @@ for (outcome in colnames(urines5_outcomes)) {
     
   }
 }
+
 for (i in 1:length(final_bestparams)) {
   
   param <- data.frame(final_bestparams[i])
